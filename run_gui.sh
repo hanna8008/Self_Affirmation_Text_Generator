a1ee2780af9515dcd9419071e582fdd1734c30a0
Hanna Zelis <hannazelis@gmail.com>
2025-05-26 13:05:32 -0500

    finalized run_gui.sh

--- run_gui.sh
+++ run_gui.sh
@@ -1,5 +1,18 @@
+# ----------------------------------------------------------------------------
+# run_gui.sh
+# ----------------------------------------------------------------------------
+#
+# This shell script launches the Gradio-baed gui ('gui.py') for generating 
+# affirmations. It's desgined to be run from the root project directory.
+
+
+
 #!/bin/bash
 
 #source ~/miniconda/etc/profile.d/conda.sh
 #conda activate affirmgen
+
+
+
+#runs the python script that launches the Gradio web app interface
 python gui.py
